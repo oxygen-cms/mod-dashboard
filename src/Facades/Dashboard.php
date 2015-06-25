45034f66473eb1660ae64b751b379d6dd8c23a58
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-25 15:42:43 +1000

    Fixed facade

--- src/Facades/Dashboard.php
+++ src/Facades/Dashboard.php
@@ -4,10 +4,12 @@ namespace OxygenModule\Dashboard\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+use OxygenModule\Dashboard\Dashboard as DashboardManager;
+
 class Dashboard extends Facade {
 
     protected static function getFacadeAccessor() {
-        return 'oxygen.dashboard';
+        return DashboardManager::class;
     }
 
 }
